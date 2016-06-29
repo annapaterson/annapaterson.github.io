6b066ad18946ba85885f863b8bf2852edb32cceb
TheoLeanse <theoleanse@gmail.com>
2016-06-29 18:10:05 +0100

    nice left alignment

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ bkgroundImageTarget.innerHTML = `<img class="pic-two main-picture"
                              images/two/picture-two-fs8_vkb8w2_c_scale,w_1044.png 1044w,
                              images/two/picture-two-fs8_vkb8w2_c_scale,w_1284.png 1284w,
                              images/two/picture-two-fs8_vkb8w2_c_scale,w_1400.png 1400w"
-                     src="images/two/picture-two-fs8_vkb8w2_c_scale,w_1400.png"
+                     src="images/two/picture-two-fs8_vkb8w2_c_scale,w_720.png"
                      alt="">`;
 
 const picOne = document.querySelector('.pic-one');
