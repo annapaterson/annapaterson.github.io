07a9d3c1435e2d6e70082a62034540a065fcab04
TheoLeanse <theoleanse@gmail.com>
2016-06-29 22:17:26 +0100

    rename images SEO

--- main.js
+++ main.js
@@ -2,13 +2,13 @@ const bkgroundImageTarget = document.querySelector('.bkgroundImageTarget');
 bkgroundImageTarget.innerHTML = `<img class="pic-two main-picture"
                      sizes="(min-width: 725px) 50vw, 100vw"
                      srcset="
-                             images/two/picture-two-fs8_vkb8w2_c_scale,w_200.png 200w,
-                             images/two/picture-two-fs8_vkb8w2_c_scale,w_720.png 720w,
-                             images/two/picture-two-fs8_vkb8w2_c_scale,w_1044.png 1044w,
-                             images/two/picture-two-fs8_vkb8w2_c_scale,w_1284.png 1284w,
-                             images/two/picture-two-fs8_vkb8w2_c_scale,w_1400.png 1400w"
-                     src="images/two/picture-two-fs8_vkb8w2_c_scale,w_720.png"
-                     alt="">`;
+                             images/two/anna-paterson-2-w_200.png 200w,
+                             images/two/anna-paterson-2-w_720.png 720w,
+                             images/two/anna-paterson-2-w_1044.png 1044w,
+                             images/two/anna-paterson-2-w_1284.png 1284w,
+                             images/two/anna-paterson-2-w_1400.png 1400w"
+                     src="images/two/anna-paterson-2-w_720.png"
+                     alt="Anna Paterson">`;
 
 const picOne = document.querySelector('.pic-one');
 const picTwo = document.querySelector('.pic-two');
